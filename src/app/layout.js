a985d57bc55c7b44ecaef9e434b43e51649f7056
jdquinterocg <jdquinterocg@gmail.com>
2024-06-16 18:01:22 -0500

    First commit with UI

---
+++ src/app/layout.js
@@ -0,0 +1,30 @@
+// This is the root layout component for your Next.js app.
+// Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
+
+import { Libre_Franklin } from 'next/font/google'
+import { Taviraj } from 'next/font/google'
+import './styles.css'
+import { Component } from '@/components/component/component'
+
+const libre_franklin = Libre_Franklin({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-libre_franklin',
+})
+const taviraj = Taviraj({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-taviraj',
+  weight: ["400", "500", "600", "700", "800", "900"],
+})
+
+export default function Layout({ children }) {
+  return (
+    <html lang="en">
+      <body className={libre_franklin.variable + ' ' + taviraj.variable}>
+        {/* <Component /> */}
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+import { Component } from "@/components/component/component"
+import './globals.css';
+
+export default function Home() {
+  return (
+      <Component />
+  )
+}
