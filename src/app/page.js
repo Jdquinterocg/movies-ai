0610bcf97f2a36f1ed5ff632cb6451392c70d542
jdquinterocg <jdquinterocg@gmail.com>
2024-06-19 21:36:47 -0500

    ADD data to frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,45 @@
+"use client";
+
 import { Component } from "@/components/component/component"
 import './globals.css';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+
+  const [movies, setMovies] = useState([]);
+  const [request, setRequest] = useState('');
+
+  useEffect(() => {
+    // Fetch movies data from API
+    const fetchMovies = async () => {
+      const response = await fetch('/api', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ request }),
+      });
+      const data = await response.json();
+      console.log(data);
+      setMovies(data);
+    };
+
+    fetchMovies();
+  }, [request]);
+
+  const handleSearch = (query) => {
+    console.log(query)
+    setRequest(query); // Actualiza la consulta en el estado
+    
+  };
+
+  const resetSearch = () => {
+    setRequest('');
+  };
+
   return (
-      <Component />
+    <Component
+      movies={movies}
+      handleSearch={handleSearch}
+      resetSearch={resetSearch}
+    />
   )
 }
